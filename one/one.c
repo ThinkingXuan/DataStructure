d58fa7f8c543a9e1ea1d8a55b13494d7d0bd36a9
ThingingXuan <1454739828@qq.com>
2019-01-16 15:53:04 +0800

    commit

---
+++ one/one.c
@@ -0,0 +1,21 @@
+//时间复杂度 
+
+int a = 1;
+int b = 2;
+int sum = a + b;
+
+
+void fun(){
+	int i,sum;          //执行一次 
+	for(i=0;i<n;i++){
+		sum += i;       //执行n次 
+	}
+	printf("%d\n",sum); //执行一次
+}
+
+void fun(){
+	int i = 1;
+	while(i < n){
+		i * = 2;
+	}
+}
